f324dae0f0d4289ef0170942397596fb1fe9eb0a
avizipi <avizipi@gmail.com>
2020-05-05 23:30:11 +0300

    added a pck support to ros2 image publisher

--- image_publisher_ros2_workspace/src/image_publisher/image_publisher/image_publisher_ros2.py
+++ image_publisher_ros2_workspace/src/image_publisher/image_publisher/image_publisher_ros2.py
@@ -9,24 +9,23 @@ class Image_Publisher(Node):
     def __init__(self):
         super().__init__("image_tutorial")
         self.add_msg_to_info_logger("initializing node")
-        self.image_publisher_ = self.create_publisher(Image, 'image', 1)
+        self.image_publisher_ = self.create_publisher(Image, 'image', 10)
         self.bridge = CvBridge()
 
         
     def publish(self, cv2_image):
-        self.add_msg_to_info_logger("sending_image")
         try:
-             msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-             self.publisher_.publish(msg)
+             msg = self.bridge.cv2_to_imgmsg(cv2_image, "bgr8")
+             self.image_publisher_.publish(msg)
         except CvBridgeError as e:
              self.add_msg_to_info_logger(e)
+        self.add_msg_to_info_logger("sending_image")
         
 
     def add_msg_to_info_logger(self, msg):
         self.get_logger().info(msg)
         
-
-rclpy.init(None)
+rclpy.init()
 image_publisher = Image_Publisher()
 
 image_publisher.add_msg_to_info_logger("initializing camera")
@@ -36,19 +35,19 @@ cv2.namedWindow("image_tutorial")
 while True:
     ret, frame = cam.read()
     if not ret:
-        image_publisher.add_msg_to_info_logger("something went wrong. camera is offline?")
-        break
-        
+    	image_publisher.add_msg_to_info_logger("something went wrong. camera is offline?")
+    	break
+	
     cv2.imshow("image_tutorial", frame)
     pressed = cv2.waitKey(1)
-    if pressed == chr(27).encode() or pressed == ord('q'):
-        # closing application if esc or q pressed
-        image_publisher.add_msg_to_info_logger("closing button pressed, closing")
-        break
-        
-    elif pressed == chr(32).encode():
-        # publishing image if space pressed
-        image_publisher.publish(frame)
+    if pressed == 27 or pressed == ord('q'):
+	# closing application if esc or q pressed
+    	image_publisher.add_msg_to_info_logger("closing button pressed, closing")
+    	break
+	
+    elif pressed == 32:
+	# publishing image if space pressed
+    	image_publisher.publish(frame)
 
 # closing  
 image_publisher.destroy_node()
